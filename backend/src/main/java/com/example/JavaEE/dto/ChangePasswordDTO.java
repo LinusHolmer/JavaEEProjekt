2d49b288db2a1bdfdce6129755f6885bca3ddd3a
LinusHolmer <linus.holmer@stud.sti.se>
2025-12-03 19:08:04 +0100

    Update ChangePasswordDTO.java

--- backend/src/main/java/com/example/JavaEE/dto/ChangePasswordDTO.java
+++ backend/src/main/java/com/example/JavaEE/dto/ChangePasswordDTO.java
@@ -3,8 +3,10 @@ package com.example.JavaEE.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record ChangePasswordDTO(@Size(max = 50, message = "Maximum characters is 50")@NotBlank(message = "Username can not be Blank")
-                                String username,
-                                @Size(max = 50, message = "Maximum characters is 50")@NotBlank(message = "Password can not be Blank")
-                                String newPassword) {
+public record ChangePasswordDTO(
+        @Size(max = 50, message = "Maximum characters is 50")@NotBlank(message = "Username can not be Blank")
+        String username,
+
+        @Size(max = 50, message = "Maximum characters is 50")@NotBlank(message = "Password can not be Blank")
+        String newPassword) {
 }
