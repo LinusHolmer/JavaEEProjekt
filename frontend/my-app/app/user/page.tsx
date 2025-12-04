843deb1a0399dbe404bf67e9951c9c3773b17560
LinusHolmer <linus.holmer@stud.sti.se>
2025-12-04 15:42:02 +0100

    ADD: new API for requests
    API for fetch requests.

--- frontend/my-app/app/user/page.tsx
+++ frontend/my-app/app/user/page.tsx
@@ -1,16 +1,23 @@
-
+"use client";
+import { useEffect, useState } from "react";
 
 export default function UserPage() {
-  
+  const [users, setUsers] = useState<any[]>([]);
 
-    return (
+  useEffect(() => {
+    fetch("/api/user", { credentials: "include" })
+      .then(response => response.json())
+      .then(setUsers);
+  }, []); 
+
+  return (
     <main>
-    <h1>USER PAGE</h1>
-
-
-
+      <h1>User Page</h1>
+      <ul>
+        {users.map((user, i) => (
+          <li key={i}>{user.username}: {user.roles}</li>
+        ))}
+      </ul>
     </main>
   );
-  };
-
-  
+}
